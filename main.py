779a91688091545c0f18753b04f4411eb8b135b2
nikhildhage <nikhil.dhage@gmail.com>
2023-09-17 19:46:33 -0700

    Setup charts folder to build if missing

--- main.py
+++ main.py
@@ -43,6 +43,11 @@ def getClosing(ticker: str) -> list:
 
 
 def showGraphs():
+    try:
+        Path('charts').mkdir()
+    except FileExistsError:
+        pass
+
     # stocks list
     stocks = ["MSFT", "AAPL", "GME", "SONY", "META"]
 
